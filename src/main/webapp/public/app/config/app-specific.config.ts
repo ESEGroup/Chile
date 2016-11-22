b84f7e7466aa6f68c5dd14d33579b710c2e96de3
Eric Reis Figueiredo <eric_reisfig@poli.ufrj.br>
2016-11-22 11:11:05 -0200

    [API] [Jenkins] Changing API_URL for jenkins

--- src/main/webapp/public/app/config/app-specific.config.ts
+++ src/main/webapp/public/app/config/app-specific.config.ts
@@ -1,4 +1,4 @@
 namespace App.Config {
 	export const MODULE_NAME: string = 'app';
-	export var API_URL: string = 'http://localhost:8080';
+	export var API_URL: string = 'http://localhost:8081/manutencao-ufrj';
 }
