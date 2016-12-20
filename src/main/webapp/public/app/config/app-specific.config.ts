d150117fd4eccf6e2e093da1a24a346a6512dae3
Eric Reis Figueiredo <eric_reisfig@poli.ufrj.br>
2016-12-19 23:29:36 -0200

    [WebApp] Fixing API url for AWS

--- src/main/webapp/public/app/config/app-specific.config.ts
+++ src/main/webapp/public/app/config/app-specific.config.ts
@@ -1,4 +1,5 @@
 namespace App.Config {
 	export const MODULE_NAME: string = 'app';
-	export var API_URL: string = 'http://localhost:8081/manutencao-ufrj';
+	// export var API_URL: string = 'http://localhost:8081/manutencao-ufrj';
+	export var API_URL: string = 'http://ec2-34-192-86-33.compute-1.amazonaws.com:8081/diretodafont';
 }
